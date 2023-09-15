612aac50c9532d8284ba6201bfc8e470cd0a9dda
ch052877 <christyanmend@gmail.com>
2023-09-15 01:55:35 -0300

    creating display filter

--- spotfy.py
+++ spotfy.py
@@ -14,18 +14,23 @@ df = pd.read_csv("01_Spotify.csv")
 with aba1:
     st.dataframe(df)
 with aba2:
-    # coluna1, coluna2 = st.columns(2)
-    # with coluna1:
     df.set_index("Track", inplace=True)
-
-    artists = df["Artist"].value_counts().index
-
-    artist = st.selectbox("Artista", artists)
-
-    df_filtered = df[df["Artist"] == artist]
-
-    st.bar_chart(df_filtered["Stream"])
-    # df
-    # with coluna2:
-with aba3:
     
+    artists = df["Artist"].value_counts().index
+    artist = st.sidebar.selectbox("Artista", artists)
+    df_filtered = df[df["Artist"] == artist]
+    display_songs = st.sidebar.checkbox('Display Songs')
+    albuns = df_filtered["Album"].value_counts().index
+    album = st.sidebar.selectbox("Album", albuns)
+    df_filtered_albuns = df[df["Album"] == album]
+    display_albuns = st.sidebar.checkbox('Display Dashboard')
+
+    if display_songs:
+        st.write(artist)
+        st.bar_chart(df_filtered["Stream"])
+    if display_albuns:
+        st.bar_chart(df_filtered_albuns["Album"])
+
+with aba3:
+    df.set_index("Artist", inplace=True)
+    st.bar_chart(df[df["Stream"] > 1_500_000_000]["Stream"])
